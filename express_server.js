a91319af3cfe8a08520993ae599b0f8eee22a48c
Susan Zhang <susanwenzhang@gmail.com>
2019-12-10 17:50:00 +0000

    committing changes before starting day 2 work on tinyapp

--- express_server.js
+++ express_server.js
@@ -33,8 +33,20 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString()
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log("Current urlDatabase:\n", urlDatabase);
+  res.redirect(`/urls/${shortURL}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[shortURL];
+  if (!longURL.startsWith("http")) {
+    longURL = "https://" + longURL;
+  }
+  res.redirect(longURL);
+  console.log(longURL);
 });
 
 app.get("/urls/new", (req, res) => {
